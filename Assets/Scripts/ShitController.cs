8c0ee14ae3bce4b0bc9255446a3828c5cc472dd5
Vratislav Kalenda <v.kalenda@gmail.com>
2015-01-14 00:46:56 +0100

    Drunk has its own collision layer

--- Assets/Scripts/ShitController.cs
+++ Assets/Scripts/ShitController.cs
@@ -5,10 +5,13 @@ public class ShitController : MonoBehaviour
 {
 
     private Animator anim;
+	private ShitSpawner shitSpawner;
+	public Transform ShitPoint;
     // Use this for initialization
     void Start()
     {
         anim = GetComponent<Animator>();
+		shitSpawner = GameObject.Find ("ShitSpawner").GetComponent<ShitSpawner> ();
     }
     // Update is called once per frame
     void Update()
@@ -24,6 +27,7 @@ public class ShitController : MonoBehaviour
     public void spawnShit()
     {
         Debug.Log("Spawn the shit right here!");
+		shitSpawner.SpawnShitAtPoint (ShitPoint.position);
     }
 
     private void takeShit()
---
+++ Assets/Scripts/ShitSpawner.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShitSpawner : MonoBehaviour, IGameStateListener {
+
+	private GameState gameState;
+	public GameObject SpawnedShit;
+	public GameObject ShitPrefab;
+	
+	void Awake(){
+		gameState = GameObject.Find ("GameState").GetComponent<GameState> ();
+		gameState.RegisterGameStateListener (this);
+	}
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	#region IGameStateListener implementation
+	
+	public void OnGameStateChange (GameStates oldStates, GameStates newState)
+	{
+		if(newState == GameStates.Intro || (newState == GameStates.Planning && oldStates != GameStates.Intro)){
+			DestroyShit();
+		}
+	}
+	
+	#endregion
+	private void DestroyShit(){
+		if (SpawnedShit != null) {
+			Destroy(SpawnedShit);
+			SpawnedShit = null;
+		}
+	}
+
+
+	public void SpawnShitAtPoint(Vector2 shitLocation){
+		DestroyShit ();
+		SpawnedShit = (GameObject) Instantiate (ShitPrefab);
+		SpawnedShit.transform.position = shitLocation;
+	}
+}
