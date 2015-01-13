8b811df008245172c6992a29d548c3f5e2b4c3f6
Vratislav Kalenda <v.kalenda@gmail.com>
2015-01-14 00:08:05 +0100

    Idle animations autoplay

--- Assets/Scripts/DrunkController.cs
+++ Assets/Scripts/DrunkController.cs
@@ -1,20 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class DrunkController : MonoBehaviour {
 
 	public float speed = 1f;
+	public float minSecondsBetweenIdle = 4;
+	public float maxSecondsBetweenIdle = 10;
+
+	private float nextIdle = 0;
+	private List<string> anims = new List<string> ();
 
 	private GameState gameState;
 
     private Animator anim;
 
 	void Awake() {
+
 		gameState = GameObject.Find ("GameState").GetComponent<GameState> ();
 	}
 
 	// Use this for initialization
 	void Start () {
+		ComputeNextIdle ();
+		Random.seed = System.DateTime.Today.Millisecond;
+		anims.Add ("Tdrink");
+		anims.Add ("Thead");
+		anims.Add ("Tbend");
+
         anim = GetComponent<Animator>();
 	}
 
@@ -35,10 +48,28 @@ public class DrunkController : MonoBehaviour {
             Hand();
        
     }
-	
+
+	private void ComputeNextIdle(){
+		nextIdle = Time.time + Random.Range (minSecondsBetweenIdle, maxSecondsBetweenIdle);
+	}
+
+	private void PlayIdle(){
+		if (Time.time >= nextIdle) {
+			if (gameState.State == GameStates.Planning || gameState.State == GameStates.Intro) {
+				int random = Random.Range(0,anims.Count-1);
+				
+			    string pickedAnimT = anims.ToArray()[random];
+				Debug.Log("Playing idle " + pickedAnimT);
+				anim.SetTrigger(pickedAnimT);
+			}		
+
+			ComputeNextIdle ();
+		}
+	}
 	
 	void FixedUpdate () {
-		 if (gameState.State == GameStates.Simulation) {
+		PlayIdle ();
+		if (gameState.State == GameStates.Simulation) {
 			rigidbody2D.velocity = new Vector2(transform.localScale.x * speed, rigidbody2D.velocity.y);
             Walk();
 		}
