6d11af6c615b2028189bf57a2314b704593ccb3d
Vratislav Kalenda <v.kalenda@gmail.com>
2015-01-13 20:44:53 +0100

    Added dog detector to cat

---
+++ Assets/Scripts/DogDetector.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DogDetector : MonoBehaviour {
+
+	public GameObject DetectedDog;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		
+		if(other.gameObject != null && other.gameObject.tag == "Dog" && other.isTrigger == false){
+			Debug.Log("DOG detector collision!");
+			DetectedDog = other.gameObject;
+		}
+		
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if(other.gameObject != null && other.gameObject.tag == "Dog" && other.isTrigger == false){
+			Debug.Log("DOG detector - dog left!");
+			DetectedDog = null;
+		}
+	}
+
+}
